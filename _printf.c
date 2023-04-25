93fab672c07eab38b004c37b049c748a45bbde1a
SilVeRMEP <abdousami855@gmail.com>
2023-04-25 05:49:44 -0700

    000/

--- _printf.c
+++ _printf.c
@@ -1,6 +1,3 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -8,55 +5,42 @@
  * @format: character string containing zero or more directives
  * Return: the number of characters printed (excluding the null byte)
  */
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
-    char buffer[1024];
-    int buffer_len = 0;
-    char c;
+int _printf(const char *format, ...)
+{
+	int count = 0, c;
+	va_list args;
+	va_start(args, format);
 
-    while ((c = *format++) != '\0') {
-        if (c == '%') {
-            switch (*format++) {
-                case 'c':
-                    buffer[buffer_len++] = (char)va_arg(args, int);
-                    break;
-                case 's': {
-                    const char *s = va_arg(args, const char *);
-                    int len = strlen(s);
-                    if (buffer_len + len > 1023) {
-                        count += buffer_len;
-                        buffer[buffer_len] = '\0';
-                        printf("%s", buffer);
-                        buffer_len = 0;
-                    }
-                    strcpy(buffer + buffer_len, s);
-                    buffer_len += len;
-                    break;
-                }
-                case '%':
-                    buffer[buffer_len++] = '%';
-                    break;
-                default:
-                    va_end(args);
-                    return -1;
+	while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                c = va_arg(args, int);
+                count += putchar(c);
             }
-        } else {
-            buffer[buffer_len++] = c;
+            else if (*format == 's')
+            {
+                count += printf("%s", va_arg(args, char *));
+            }
+            else if (*format == '%')
+            {
+                count += putchar('%');
+            }
+            else
+            {
+                count += putchar('%');
+                count += putchar(*format);
+            }
         }
-        if (buffer_len == 1023) {
-            count += buffer_len;
-            buffer[buffer_len] = '\0';
-            printf("%s", buffer);
-            buffer_len = 0;
+        else
+        {
+            count += putchar(*format);
         }
-    }
-    if (buffer_len > 0) {
-        count += buffer_len;
-        buffer[buffer_len] = '\0';
-        printf("%s", buffer);
+        format++;
     }
     va_end(args);
-    return count;
+    return (count);
 }
