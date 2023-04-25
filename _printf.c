e5fdabb705b58d4088c7ea21855d27e9ef18186b
Wasli09 <wassze.1000@gmail.com>
2023-04-25 16:09:36 +0100

    llllll

--- _printf.c
+++ _printf.c
@@ -1,45 +1,44 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * _printf - produces output according to a format
- * @format: character string containing zero or more directives
- * Return: the number of characters printed (excluding the null byte)
+ * @format: format string containing the characters and the specifiers
+ * Return: length of the formatted 
  */
 int _printf(const char *format, ...)
 {
-int count = 0, c;
-va_list args;
-va_start(args, format);
-while (*format)
+int (*pfunc)(va_list, flags_t *);
+const char *p;
+va_list arguments;
+flags_t flags = {0, 0, 0};
+register int count = 0;
+va_start(arguments, format);
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+for (p = format; *p; p++)
 {
-if (*format == '%')
+if (*p == '%')
 {
-format++;
-if (*format == 'c')
+p++;
+if (*p == '%')
 {
-c = va_arg(args, int);
-count += putchar(c);
+count += _putchar('%');
+continue;
 }
-else if (*format == 's')
-{
-count += printf("%s", va_arg(args, char *));
+while (get_flag(*p, &flags))
+p++;
+pfunc = get_print(*p);
+count += (pfunc)
+? pfunc(arguments, &flags)
+: _printf("%%%c", *p);
+} else
+count += _putchar(*p);
 }
-else if (*format == '%')
-{
-count += putchar('%');
-}
-else
-{
-count += putchar('%');
-count += putchar(*format);
-}
-}
-else
-{
-count += putchar(*format);
-}
-format++;
-}
-va_end(args);
+_putchar(-1);
+va_end(arguments);
 return (count);
 }
